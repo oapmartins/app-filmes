03308a19a6d4f977139ad3112ae0a52e6c0930a7
oapmartins <octaviomartins10@gmail.com>
2021-09-29 19:51:20 -0300

    Adicionando overview do filme da pagina de detalhes do filme.

--- lib/modules/movie_detail/widget/movie_detail_content/movie_detail_content.dart
+++ lib/modules/movie_detail/widget/movie_detail_content/movie_detail_content.dart
@@ -1,5 +1,6 @@
 import 'package:app_filmes/models/movie_detail_model.dart';
 import 'package:app_filmes/modules/movie_detail/widget/movie_detail_content/movie_detail_content_title.dart';
+import 'package:app_filmes/modules/movie_detail/widget/movie_detail_content_credits.dart';
 import 'package:flutter/material.dart';
 
 class MovieDetailContent extends StatelessWidget {
@@ -13,6 +14,7 @@ class MovieDetailContent extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         MovieDetailContentTitle(movie: movie),
+        MovieDetailContentCredits(movie: movie)
       ],
     );
   }
